605a1db92c160698966b1c064e978493bab6a784
melchior <vrol0004@gmail.com>
2025-05-19 22:54:50 +0200

    project code imported

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# droiddevtipsExample
+# Droid Dev Tips examples
+
+A brief, catchy one-liner explaining what your project does.
+This repository contains a collection of simple and practical Android code examples from https://www.droiddevtips.com articles using Kotlin. Each project (module) demonstrates a specific concept or feature in Android development.
+
+Whether you’re a beginner learning the basics or an experienced developer looking for quick reference implementations, this repo serves as a handy resource for common Android patterns and best practices.
+
+## 🚀 Features
+- My goal is to offer practical tips when creating Android apps.
+
+## 📦 Installation
+
+```bash
+# Clone the repository
+git clone https://github.com/droiddevtips/droiddevtipsExample.git
+
+# Open and run project in Android Studio
+Open the project directory directly in Android and perform the following gradle tasks, clean and build.
---
+++ app/src/main/java/com/droiddevtips/examples/RetrievingAdIDMainActivity.kt
@@ -0,0 +1,129 @@
+package com.droiddevtips.examples
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.droiddevtips.examples.ui.theme.RetrievingAdIDTheme
+import com.google.android.gms.ads.identifier.AdvertisingIdClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class RetrievingAdIDMainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RetrievingAdIDTheme {
+                RetrieveAdID()
+            }
+        }
+    }
+}
+
+@Composable
+fun RetrieveAdID() {
+
+    val context = LocalContext.current
+
+    var adsID by remember {
+        mutableStateOf("-")
+    }
+
+    var trackingLimited by remember {
+        mutableStateOf(true)
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(
+            space = 16.dp,
+            alignment = Alignment.CenterVertically
+        )
+    ) {
+        Column(
+            modifier = Modifier.animateContentSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(14.dp)
+        ) {
+            Text(
+                "Advertising ID",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+            Text(adsID, fontSize = 14.sp, color = Color.LightGray)
+
+            AnimatedVisibility(
+                visible = adsID != "-",
+                enter = fadeIn(animationSpec = tween(500)),
+                exit = fadeOut()
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        "Tracking limited",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                    Text(
+                        if (trackingLimited) "Yes" else "No",
+                        fontSize = 14.sp,
+                        color = Color.LightGray
+                    )
+                }
+            }
+        }
+
+        Button(onClick = {
+            CoroutineScope(Dispatchers.IO).launch {
+
+                withContext(Dispatchers.Main) {
+                    adsID = "-"
+                    trackingLimited = false
+                }
+
+                val advertisingIDInfo = AdvertisingIdClient.getAdvertisingIdInfo(context)
+
+                withContext(Dispatchers.Main) {
+                    adsID = "${advertisingIDInfo.id}"
+                    trackingLimited = advertisingIDInfo.isLimitAdTrackingEnabled
+                }
+            }
+        }) {
+            Text("Retrieve Ad ID")
+        }
+    }
+}
