f98b31859eefdd016dc17317fec1cb41b74a20c3
melchior <vrol0004@gmail.com>
2025-09-28 15:12:36 +0200

    multi screen support additional updated added

--- multiscreensupport/src/main/java/com/droiddevtips/multiscreensupport/domain/NavControllerExtensions.kt
+++ multiscreensupport/src/main/java/com/droiddevtips/multiscreensupport/domain/NavControllerExtensions.kt
@@ -3,7 +3,7 @@ package com.droiddevtips.multiscreensupport.domain
 import androidx.navigation.NavController
 
 /**
- * These are the [NavHost] controller
+ * These are the [androidx.navigation.NavHostController] controller
  * Created by Melchior Vrolijk
  * Droid Dev Tips (c) 2025. All rights reserved.
  */
--- multiscreensupport/src/main/java/com/droiddevtips/multiscreensupport/ui/viewmodelFactoryExample/MultiScreenViewModelFactory.kt
+++ multiscreensupport/src/main/java/com/droiddevtips/multiscreensupport/ui/viewmodelFactoryExample/MultiScreenViewModelFactory.kt
@@ -1,3 +1,5 @@
+@file:Suppress("UNCHECKED_CAST")
+
 package com.droiddevtips.multiscreensupport.ui.viewmodelFactoryExample
 
 import androidx.lifecycle.ViewModel
