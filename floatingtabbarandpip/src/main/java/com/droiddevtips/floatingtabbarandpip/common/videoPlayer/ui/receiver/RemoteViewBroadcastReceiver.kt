41a3978639d2bdeb8896b980c0ba573405de0d57
melchior <vrol0004@gmail.com>
2026-01-07 23:20:29 +0100

    final code cleanup

--- floatingtabbarandpip/src/main/java/com/droiddevtips/floatingtabbarandpip/common/videoPlayer/ui/receiver/RemoteViewBroadcastReceiver.kt
+++ floatingtabbarandpip/src/main/java/com/droiddevtips/floatingtabbarandpip/common/videoPlayer/ui/receiver/RemoteViewBroadcastReceiver.kt
@@ -3,7 +3,6 @@ package com.droiddevtips.floatingtabbarandpip.common.videoPlayer.ui.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import android.widget.Toast
 
 /**
