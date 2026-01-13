1b0956c9561b5f45892fc3bd313aa4229470e293
melchior <vrol0004@gmail.com>
2026-01-13 22:49:10 +0100

    base music player project code cleanup

--- musicplayer/src/main/java/com/droiddevtips/musicplayer/MusicPlayerActivity.kt
+++ musicplayer/src/main/java/com/droiddevtips/musicplayer/MusicPlayerActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.droiddevtips.musicplayer.ui.theme.DroidDevTipsExamplesTheme
 import com.droiddevtips.typography.DroidDevTipsTheme
 
 class MusicPlayerActivity : ComponentActivity() {
