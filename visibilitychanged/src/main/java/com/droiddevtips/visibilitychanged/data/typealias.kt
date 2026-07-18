409d53d137a4847a71f136dc8067a5d5aa9df34e
melchior <vrol0004@gmail.com>
2026-07-18 17:37:28 +0200

    code cleanup

--- visibilitychanged/src/main/java/com/droiddevtips/visibilitychanged/data/typealias.kt
+++ visibilitychanged/src/main/java/com/droiddevtips/visibilitychanged/data/typealias.kt
@@ -2,4 +2,10 @@ package com.droiddevtips.visibilitychanged.data
 
 import com.droiddevtips.visibilitychanged.R
 
+/**
+ * Typealias used throughout this demo app.
+ * Provides a single, stable reference point to various app references.
+ * Created by Melchior Vrolijk
+ * Droid Dev Tips (c) 2026. All rights reserved.
+ */
 typealias AppString = R.string
